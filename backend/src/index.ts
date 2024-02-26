bb0cb70793fb21ef42d1da0cb4b50780da2cf585
Vladyslav Vereta <vlvereta.92@gmail.com>
2024-02-26 22:20:30 +0200

    Initial setup.

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World from Backend!'));
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
